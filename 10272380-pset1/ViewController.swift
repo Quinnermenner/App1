d3ca836f3c957702282a6800834ce09dc315231f
Quinten van der Post <Qvdpost@quintens-macbook-air.local>
2016-11-03 23:52:49 +0100

    added first switch

--- 10272380-pset1/ViewController.swift
+++ 10272380-pset1/ViewController.swift
@@ -14,6 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var arms: UIImageView!
+    @IBOutlet weak var hat: UIImageView!
+    @IBOutlet weak var ears: UIImageView!
+    @IBOutlet weak var mouth: UIImageView!
+    @IBOutlet weak var eyes: UIImageView!
+    @IBOutlet weak var shoes: UIImageView!
+    @IBOutlet weak var mustache: UIImageView!
+    @IBOutlet weak var nose: UIImageView!
+    @IBOutlet weak var eyebrows: UIImageView!
+    @IBOutlet weak var glasses: UIImageView!
+    
+    @IBAction func toggle_arms(_ sender: UISwitch) {
+        if arms.isHidden == true
+        {
+            arms.isHidden = false;
+        }
+        else
+        {
+            arms.isHidden = true;
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
